c94dea8138e96d1d7392b64077140e6eca8c387f
enayat789 <enayatbhai2006@gmail.com>
2023-08-26 23:24:22 +0530

    Password hide and show added

--- script.js
+++ script.js
@@ -2,32 +2,61 @@ function loginForm() {
   let emailid = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
-  if (
-    (emailid == "" || emailid == null) &&
-    (password == "" || password == null)
-  ) {
+  if (emailid === "" && password === "") {
     alert("Please enter email and password!! , it can't be empty!!");
-    return false;
-  } else if (emailid == "" || emailid == null) {
-    alert("Please enter emailid. It can't be empty!! ");
-    return false;
-  } else if (password == "" || password == null) {
-    alert("Please enter password. It can't be empty!! ");
-    return false;
-  }
-
-  // checking the emailId(@)
-  const checkEmail = emailid.indexOf("@gmail.com");
-  if (checkEmail == -1) {
-    alert("email is not valid");
-    return false;
-  }
-
-  // checking the password length
-  const passwordCheck = password.length;
-  if (passwordCheck < 8) {
-    alert("Password length must be atleast 8 characters");
+  } else if (emailid == "") {
+    // checking the password length
+    const passwordCheck = password.length;
+    if (passwordCheck < 8) {
+      alert(
+        "Please enter emailid and Password length must be atleast 8 characters"
+      );
+    } else {
+      alert("Please enter emailid. It can't be empty!! ");
+    }
+  } else if (password == "") {
+    // checking the emailId(@)
+    const checkEmail = emailid.indexOf("@gmail.com");
+    if (checkEmail == -1) {
+      alert(
+        "email is not valid and Please enter password. It can't be empty!! "
+      );
+    } else {
+      alert("Please enter password. It can't be empty!! ");
+    }
   } else {
-    alert(emailid + "\n" + password);
+    // checking the emailId(@)
+    const checkEmail = emailid.indexOf("@gmail.com");
+    const passwordCheck = password.length;
+
+    if (checkEmail == -1) {
+      const passwordCheck = password.length;
+      if (passwordCheck < 8) {
+        alert(
+          " email is not valid and Password length must be atleast 8 characters"
+        );
+      } else {
+        alert("email is not valid");
+      }
+    } else if (passwordCheck < 8) {
+      alert("Password length must be atleast 8 characters");
+    } else {
+      alert(emailid + "\n" + password);
+    }
+  }
+
+  // *******
+}
+
+function changePasswordVisibility() {
+  const passwordInput = document.getElementById("password");
+  const passwordIcon = document.getElementById("passwordIcon");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordIcon.src = "./images/iconShowPassword.png";
+  } else {
+    passwordInput.type = "password";
+    passwordIcon.src = "./images/iconHidePassword.png";
   }
 }

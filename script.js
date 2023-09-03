0be164a3dcc04d644dc4a04850fdf3faabbc2b69
enayat789 <enayatbhai2006@gmail.com>
2023-09-03 14:32:42 +0530

    image file saved to local storage

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 //login
-function loginForm() {
-  let emailid = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
-  let contactNumber = document.getElementById("phNumber").value;
-  let profilePicture = document.getElementById("avatar").value;
-  // console.log(profilePicture);
+async function loginForm() {
+  const userName = document.getElementById("userName").value;
+  const emailid = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const contactNumber = document.getElementById("phNumber").value;
+  const profileInput = document.getElementById("profile").files[0];
 
   let passwordCheck = password.length;
   let contactCheck = contactNumber.length;
   let checkEmail = emailid.includes("@gmail.com");
 
-  if (emailid === "" && password === "") {
+  if (userName == "") {
+    const msg = "Please enter your Full Name!! ";
+    showToast(msg, "invalid");
+    //
+  } else if (emailid === "" && password === "") {
     const msg = "Please enter email and password!! , it can't be empty!!";
     showToast(msg, "error");
-  } else if (emailid == "" || password == "") {
+    //
+  } else if (emailid == "" || password == "" || userName == "") {
     if (emailid == "") {
       const msg = "Please enter emailid. It can't be empty!! ";
       showToast(msg, "error");
@@ -38,7 +43,6 @@ function loginForm() {
         const msg = "email is not valid";
         showToast(msg, "invalid");
       }
-      //
     } else if (passwordCheck < 8) {
       const msg = "Password length must be atleast 8 characters";
       showToast(msg, "invalid");
@@ -49,17 +53,29 @@ function loginForm() {
       const msg = "Invalid Contact Number!! ";
       showToast(msg, "invalid");
     } else {
-      const payload = { emailid, password, contactNumber }; // creating a new object
+      try {
+        const profileImageBase64 = await fileToBase64(profileInput);
+        const payload = {
+          userName,
+          emailid,
+          password,
+          contactNumber,
+          profileImage: profileImageBase64, // Use the external variable
+        }; // creating a new object
 
-      if (!localStorage.getItem("credentials")) {
-        localStorage.setItem("credentials", JSON.stringify(payload));
-      } else {
-        alert("user alredy created");
+        if (!localStorage.getItem("credentials")) {
+          localStorage.setItem("credentials", JSON.stringify(payload));
+        } else {
+          alert("user alredy created");
+        }
+        const containerDiv = document.getElementById("parentContainer");
+        containerDiv.style.display = "flex";
+        // createView();
+        createNavbar();
+      } catch (error) {
+        // if any error,
+        console.log("err haii kuch !!");
       }
-      const containerDiv = document.getElementById("parentContainer");
-      containerDiv.style.display = "flex";
-      // createView();
-      createNavbar();
     }
   }
 }
@@ -70,17 +86,16 @@ window.addEventListener("load", function () {
   if (localStorage.getItem("credentials")) {
     const containerDiv = document.getElementById("parentContainer");
     containerDiv.style.display = "flex";
-    createView();
+    // createView();
     createNavbar();
   }
 });
 
-// show the image from the input file
+// show the image from the input file taken from user
 
 const getImage = function (event) {
   const showImage = document.getElementById("showImage");
   showImage.style.display = "block";
-  //
 
   const uploadedImage = document.getElementById("uploadedImage");
   uploadedImage.src = URL.createObjectURL(event.target.files[0]);
@@ -89,28 +104,64 @@ const getImage = function (event) {
   };
 };
 
+// Function to convert a file to base64 using a callback
+function fileToBase64(file) {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject("No file provided");
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const base64Data = e.target.result;
+      resolve(base64Data);
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
+
 // create logged in user View--
 
+var isOpen = false; // boolean  true / false
+
+// ***
 function createView() {
   if (JSON.parse(localStorage.getItem("credentials"))) {
     const userData = JSON.parse(localStorage.getItem("credentials"));
 
     // descrtructirug
-    const { emailid, password } = userData;
+    const { emailid, userName, contactNumber, profileImage } = userData;
 
     const showData = document.getElementById("displayData");
-    showData.style.display = "flex";
+    // showData.style.display = "block";
 
     const showDiv = document.createElement("div");
     showDiv.id = "showDiv";
     showDiv.innerHTML = `
-    <p>Your Email Id: ${emailid}</p>
-    <p>Your password: ${password}</p>
+    <img src=${profileImage} id='' />
+    <p>Full Name: ${userName}</p>
+    <p>Your Email: ${emailid}</p>
+    <p>Your Contact Number: ${contactNumber}</p>
     `;
+    showData.appendChild(showDiv);
 
-    const checkData = document.getElementById("displayData").innerHTML;
-    if (checkData === "") {
-      showData.appendChild(showDiv);
+    // const checkData = document.getElementById("displayData").innerHTML;
+    // if (checkData === "") {
+    //   showData.appendChild(showDiv);
+    // }
+
+    // const showData = document.getElementById("displayData");
+    // showData.style.display = "block";
+
+    if (isOpen) {
+      isOpen = false;
+      showData.id = "displayData";
+    } else {
+      isOpen = true;
+      showData.id = "displayData_2";
     }
   }
 }
@@ -134,11 +185,16 @@ function createNavbar() {
 
   // let emailid = document.getElementById("email").value;
   const userData = JSON.parse(localStorage.getItem("credentials"));
-  const { emailid, password } = userData;
+  const { emailid, password, profileImage } = userData;
 
   const createEmailDiv = document.createElement("div");
+  const profilePic = document.createElement("img");
+  profilePic.id = "profilePic";
+  profilePic.src = profileImage;
+  profilePic.addEventListener("click", () => createView());
+
+  createEmailDiv.appendChild(profilePic);
   createEmailDiv.id = "emailDiv";
-  createEmailDiv.innerHTML = `<i class="fa-solid fa-user fa-2x " onclick="alert('Emailid: ${emailid}')"> </i>`;
 
   navbar.appendChild(createEmailDiv);
 }
